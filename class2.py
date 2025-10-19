02b57d973ed1d06d27068be5729df38f6a238a1a
AliRaza <alibzr4@gmail.com>
2025-10-19 02:26:45 -0700

    Initial commit

---
+++ class2.py
@@ -0,0 +1,33 @@
+# Variable data types => str(string), int(integer), float(decimal), bool(boolean)
+
+# String
+name : str = 'my name is Ali Raza'
+name1 : str = "Ali Raza"
+name2 : str = '''my name is
+ Ali Raza'''
+name3 : str = """my name is 
+Ali Raza"""
+# print(name)
+# print(name1)
+# print(name2)
+# print(name3)
+
+
+# Integer
+age : int = 18
+year : int = 2024
+# print(age)
+# print(year)
+print(type(age))
+
+# Float
+price : float = 20.02
+# print(price)
+
+# Boolean
+
+is_true : bool = True
+print(is_true)
+is_false : bool = False
+# print(is_true)
+# print(is_false)
